edb657f5bfdce8a11d99c7199aa0b92e309bd83e
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-04-29 20:46:19 +0300

    feat(models): Statistic class implementation

--- src/main/java/org/prog3/foot/models/Statistic.java
+++ src/main/java/org/prog3/foot/models/Statistic.java
@@ -1,4 +1,15 @@
 package org.prog3.foot.models;
 
+import lombok.Data;
+
+@Data
+/**
+ * Utilisation: GET /players/{id}/statistics/{seasonYear}
+ * Description: Obtenir les statistiques d'un joueur spécifique a une date donnée
+ */
 public class Statistic {
+    private String id;
+    private Integer scoreGoals;
+    private PlayingTime playingTime;
+
 }

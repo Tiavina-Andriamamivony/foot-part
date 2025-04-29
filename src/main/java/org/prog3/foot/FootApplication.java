c55597bcdb4d551b810ee83c48a28c4816363587
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-04-29 19:29:29 +0300

    chore: initialization of the project

---
+++ src/main/java/org/prog3/foot/FootApplication.java
@@ -0,0 +1,13 @@
+package org.prog3.foot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootApplication.class, args);
+    }
+
+}

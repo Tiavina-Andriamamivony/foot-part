02c8cb2aa177274d0a2c0bce5f83ca5e2cff2f7c
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-04-30 12:18:04 +0300

    chore(coach): Droping id field .

--- src/main/java/org/prog3/foot/models/Coach.java
+++ src/main/java/org/prog3/foot/models/Coach.java
@@ -3,8 +3,8 @@ package org.prog3.foot.models;
 import lombok.Data;
 
 @Data
+//Utilisation de name et nationality en tant que clés primaire même si j'aurais utiliser un id
 public class Coach {
-    private String id;
     private String name;
     private String nationality;
 }

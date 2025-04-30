132f2bc65b8c1cb19e947b1bd7911b30c6c8c12d
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-04-30 12:44:20 +0300

    feat(Creat models): Create season model.

---
+++ src/main/java/org/prog3/foot/models/CreateSeason.java
@@ -0,0 +1,18 @@
+package org.prog3.foot.models;
+
+import lombok.Data;
+
+/**
+ * For example, if season.year = 2024, alias would be "S2024-2025".
+ * If season.year = 2025, alias wold be "S2025-2026".
+ */
+@Data
+public class CreateSeason {
+        private Integer year;
+        private String alias;
+
+        public boolean GenerateSeasonAlias(){
+            setAlias("S"+year+"-"+year+1);
+            return true;
+        }
+}

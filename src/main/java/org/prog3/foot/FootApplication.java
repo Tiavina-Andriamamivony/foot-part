c49ab5f4b70fa6bfa586c1238230967e8de3f871
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-04-30 18:24:28 +0300

    chore(FootApplication): Add the componentScan annotation.

--- src/main/java/org/prog3/foot/FootApplication.java
+++ src/main/java/org/prog3/foot/FootApplication.java
@@ -2,8 +2,10 @@ package org.prog3.foot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.prog3.foot","org.prog3.foot.controllers"})
 public class FootApplication {
 
     public static void main(String[] args) {

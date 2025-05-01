90a40bda111cab4ad812a2cdd9db12455a419c58
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-05-01 19:51:09 +0300

    feat(service): getClubPlayer method in PlayerService.

--- src/main/java/org/prog3/foot/service/PlayerService.java
+++ src/main/java/org/prog3/foot/service/PlayerService.java
@@ -1,4 +1,19 @@
 package org.prog3.foot.service;
 
+import lombok.AllArgsConstructor;
+import org.prog3.foot.models.ClubPlayer;
+import org.prog3.foot.repository.PlayerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class PlayerService {
+    private final PlayerRepository repository;
+
+    public List<ClubPlayer> getClubPlayers(){
+        return repository.getClubPlayers();
+    }
+
 }

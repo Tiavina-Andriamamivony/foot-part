e4761a8fdea8786a643212471cdcc4a41b3b0aa1
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-05-02 14:20:43 +0300

    feat(repository): Set implementation of Season's methods.

--- src/main/java/org/prog3/foot/repository/SeasonRepository.java
+++ src/main/java/org/prog3/foot/repository/SeasonRepository.java
@@ -1,4 +1,13 @@
 package org.prog3.foot.repository;
 
+import org.prog3.foot.models.CreateSeason;
+import org.prog3.foot.models.Season;
+import org.prog3.foot.models.UpdateSeasonStatus;
+
+import java.util.List;
+
 public interface SeasonRepository {
+    public List<Season> GetSeasons();
+    public List<Season> AddSeasons(List<CreateSeason> seasons);
+    public Season changeSeasonStatus(UpdateSeasonStatus pretending, Integer seasonYear);
 }
--- src/main/java/org/prog3/foot/repository/implementation/SeasonRepositoryImplementation.java
+++ src/main/java/org/prog3/foot/repository/implementation/SeasonRepositoryImplementation.java
@@ -1,4 +1,43 @@
 package org.prog3.foot.repository.implementation;
 
-public class SeasonRepositoryImplementation {
+import lombok.AllArgsConstructor;
+import org.prog3.foot.configuration.DataSource;
+import org.prog3.foot.models.CreateSeason;
+import org.prog3.foot.models.Season;
+import org.prog3.foot.models.UpdateSeasonStatus;
+import org.prog3.foot.repository.SeasonRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+@AllArgsConstructor
+public class SeasonRepositoryImplementation implements SeasonRepository {
+    private final DataSource dataSource;
+
+    /**
+     * @return
+     */
+    @Override
+    public List<Season> GetSeasons() {
+        return List.of();
+    }
+
+    /**
+     * @param seasons
+     * @return
+     */
+    @Override
+    public List<Season> AddSeasons(List<CreateSeason> seasons) {
+        return List.of();
+    }
+
+    /**
+     * @param pretending
+     * @param seasonYear
+     * @return
+     */
+    @Override
+    public Season changeSeasonStatus(UpdateSeasonStatus pretending, Integer seasonYear) {
+        return null;
+    }
 }

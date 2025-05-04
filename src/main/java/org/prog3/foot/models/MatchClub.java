e5823a77ea876b13064b74db6ff8e9a58e6313c3
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-05-04 14:16:55 +0300

    fix(model): MatchClub missing attributes.

--- src/main/java/org/prog3/foot/models/MatchClub.java
+++ src/main/java/org/prog3/foot/models/MatchClub.java
@@ -3,10 +3,13 @@ package org.prog3.foot.models;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
+
 @Data
 public class MatchClub implements Serializable {
     private String id;
     private String name;
     private String acronym;
-
+    private Integer score;
+    private List<Scorer> scorers;
 }
